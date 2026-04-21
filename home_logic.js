b31f0b91bb9f02bae72b7fbe8cb0d58f008f05f3
ghadeer <ghador377@gmail.com>
2026-04-21 11:28:20 +0300

    Update home_logic.js

--- home_logic.js
+++ home_logic.js
@@ -1,6 +1,3 @@
-// ===============================
-// 1) تهيئة Supabase
-// ===============================
 const supabaseUrl = "https://mcxdjisoyqdnyieboqpy.supabase.co";
 const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1jeGRqaXNveXFkbnlpZWJvcXB5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU0MDI3NDAsImV4cCI6MjA5MDk3ODc0MH0.kIwJ1pc-GLX1lTvstLSDZ5Li1rd9vwutI4-Wz_HR6nE";
 const supabase = supabase.createClient(supabaseUrl, supabaseKey);
